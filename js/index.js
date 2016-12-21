58713c5af729b52819dfff0c994e759354f7e132
Kevin Lim <knlim95@gmail.com>
2016-12-21 14:38:41 -0800

    Added anchor and attempt on jQuery SScrolling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
     console.log("Page Ready");
 });
+
+// Smooth Scrolling for Anchor Links
+// $(function() {
+//     $('a[href*="#"]:not([href="#"])').click(function() {
+//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//             var target = $(this.hash);
+//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            
+//             if (target.length) {
+//                 $('html, body').animate({
+//                 scrollTop: target.offset().top
+//             }, 1000);
+
+//             return false;
+//             }
+//         }
+//     });
+// });
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});

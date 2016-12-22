88c175675ceefb38d08c577f6ab9370a820916dd
Kevin Lim <knlim95@gmail.com>
2016-12-21 16:09:52 -0800

    Update to include Smooth Scrolling and fullscreen

--- js/index.js
+++ js/index.js
@@ -3,23 +3,6 @@ $(document).ready(function(){
 });
 
 // Smooth Scrolling for Anchor Links
-// $(function() {
-//     $('a[href*="#"]:not([href="#"])').click(function() {
-//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            
-//             if (target.length) {
-//                 $('html, body').animate({
-//                 scrollTop: target.offset().top
-//             }, 1000);
-
-//             return false;
-//             }
-//         }
-//     });
-// });
-
 $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
     if( target.length ) {
